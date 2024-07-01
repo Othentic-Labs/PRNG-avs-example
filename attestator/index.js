ef7af17dab05a13cf93887db9f6a7074e9d37485
0xpanicError <theyashsaraswat@gmail.com>
2024-07-01 13:45:59 +0530

    feat: elected performer

--- attestator/index.js
+++ attestator/index.js
@@ -1,7 +1,6 @@
-import express from 'express';
-import { ethers } from 'ethers';
-import dotenv from 'dotenv';
-// import abi from "./abi.json" assert { type: "json" };
+import express from "express";
+import { ethers } from "ethers";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -19,90 +18,100 @@ const rpcUrl = process.env.L2_RPC;
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 
 // The AttestationCenter contract object
-// const attestationCenterAddress = process.env.ATTESTATION_CENTER_ADDRESS;
-// const attestationCenterAbi = ;
-// const attestationCenterContract = new ethers.Contract(attestationCenterAddress, attestationCenterAbi, provider);
+const attestationCenterAddress = process.env.ATTESTATION_CENTER_ADDRESS;
+const attestationCenterAbi = [
+  "function numOfOperators() view returns (uint256)",
+  "function getOperatorPaymentDetail(uint256) view returns (address, uint256, uint256, uint8)",
+];
+const attestationCenterContract = new ethers.Contract(
+  attestationCenterAddress,
+  attestationCenterAbi,
+  provider
+);
 
-// /**
-//   * Find the elected task performer for a certain block
-//   */
-// async function electedLeader (blockNumber) {
-//     const count = await attestationCenterContract.numOfOperators({ blockTag: blockNumber });
-//     const selectedOperatorId = (BigInt(blockNumber) % count) + 1n;
-//     const paymentDetails = await attestationCenterContract.getOperatorPaymentDetails(selectedOperatorId, { blockTag: blockNumber });
-//     return paymentDetails[0];
-// }
-let blah = 0;
 /**
-  * Performing tasks:
-  * The "Task Performer" is chosen in a "Round Robin" fashion, meaning the
-  * operators perform tasks in the order of their IDs: 1, 2, 3, ... etc.
-  *
-  * The round-robin scheme is trivially implemented by taking the block
-  * number modulo the number of operators (plus 1). This gives us a number in
-  * the range [1..count], which we use as the ID of the chosen performer.
-  */
-provider.on('block', async (blockNumber) => {
-    // Every operator knows who is supposed to send a task in the next block
-    // const currentPerformer = await electedLeader(blockNumber);
-    const currentPerformer = "0x7D3b0D0D3883A2EeE68Fcc0D242e945dD6eEF6bb";
+ * Find the elected task performer for a certain block
+ */
+async function electedLeader(blockNumber) {
+  const count = await attestationCenterContract.numOfOperators({
+    blockTag: blockNumber,
+  });
+  const selectedOperatorId = (BigInt(blockNumber) % count) + 1n;
+  const paymentDetails =
+    await attestationCenterContract.getOperatorPaymentDetail(
+      selectedOperatorId,
+      { blockTag: blockNumber }
+    );
+  return paymentDetails[0];
+}
 
-    // If the current performer is the operator itself, it performs the task
-    if (currentPerformer === nodeAccount.address && blah === 0) {
-        console.log(`Performing task for block ${blockNumber}...`);
-        const proofOfTask = `${blockNumber}+${Date.now()}`;
-        const taskDefinitionId = 0;
-        const data = ethers.hexlify(ethers.toUtf8Bytes("hello world"));
-        const message = ethers.AbiCoder
-            .defaultAbiCoder()
-            .encode(
-                ["string", "bytes", "address", "uint16"],
-                [proofOfTask, data, nodeAccount.address, taskDefinitionId]
-            );
-        const messageHash = ethers.keccak256(message);
-        const sig = nodeAccount.signingKey.sign(messageHash).serialized;
+/**
+ * Performing tasks:
+ * The "Task Performer" is chosen in a "Round Robin" fashion, meaning the
+ * operators perform tasks in the order of their IDs: 1, 2, 3, ... etc.
+ *
+ * The round-robin scheme is trivially implemented by taking the block
+ * number modulo the number of operators (plus 1). This gives us a number in
+ * the range [1..count], which we use as the ID of the chosen performer.
+ */
+provider.on("block", async (blockNumber) => {
+  // Every operator knows who is supposed to send a task in the next block
+  const currentPerformer = await electedLeader(blockNumber);
 
-        console.log(`Performing task with seed: ${proofOfTask}`);
+  // If the current performer is the operator itself, it performs the task
+  if (currentPerformer === nodeAccount.address) {
+    console.log(`Performing task for block ${blockNumber}...`);
+    const proofOfTask = `${blockNumber}+${Date.now()}`;
+    const taskDefinitionId = 0;
+    const data = ethers.hexlify(ethers.toUtf8Bytes("hello world"));
+    const message = ethers.AbiCoder.defaultAbiCoder().encode(
+      ["string", "bytes", "address", "uint16"],
+      [proofOfTask, data, nodeAccount.address, taskDefinitionId]
+    );
+    const messageHash = ethers.keccak256(message);
+    const sig = nodeAccount.signingKey.sign(messageHash).serialized;
 
-        const jsonRpcBody = {
-            jsonrpc: "2.0",
-            method: "sendTask",
-            params: [
-              proofOfTask,
-              data,
-              taskDefinitionId,
-              nodeAccount.address,
-              sig,
-            ]
-          };
-        // The tasks consists of signing the current timestamp. The timestamp
-        // will be used as the seed for our PRNG smart contract
-        new ethers.JsonRpcProvider(NODE_RPC).send(jsonRpcBody.method, jsonRpcBody.params);
-        blah = 1;
-    }
+    console.log(`Performing task with seed: ${proofOfTask}`);
+
+    const jsonRpcBody = {
+      jsonrpc: "2.0",
+      method: "sendTask",
+      params: [proofOfTask, data, taskDefinitionId, nodeAccount.address, sig],
+    };
+    // The tasks consists of signing the current timestamp. The timestamp
+    // will be used as the seed for our PRNG smart contract
+    new ethers.JsonRpcProvider(NODE_RPC).send(
+      jsonRpcBody.method,
+      jsonRpcBody.params
+    );
+  }
 });
 
 /**
-  * AVS WebAPI endpoint:
-  * This endpoint is responsible for validating that a task was performed by
-  * the correct performer. It receives the performer from the Othentic node
-  * and checks that it's the `currentPerformer`.
-  */
-app.post('/task/validate', async (req, res) => {
-    const { proofOfTask, performer } = req.body;
-    const blockNumber = parseInt(proofOfTask.split('+')[0], 10); // Extract the block number from the proof of task
-    const electedPerformer = "0x7D3b0D0D3883A2EeE68Fcc0D242e945dD6eEF6bb"; // Get the elected performer for that block
+ * AVS WebAPI endpoint:
+ * This endpoint is responsible for validating that a task was performed by
+ * the correct performer. It receives the performer from the Othentic node
+ * and checks that it's the `currentPerformer`.
+ */
+app.post("/task/validate", async (req, res) => {
+  const { proofOfTask, performer } = req.body;
+  const blockNumber = parseInt(proofOfTask.split("+")[0], 10); // Extract the block number from the proof of task
+  const electedPerformer = electedLeader(blockNumber); // Get the elected performer for that block
 
-    console.log(`Validating task for block number: ${blockNumber}, Task Performer: ${performer}, Elected Performer: ${electedPerformer}`)
+  console.log(
+    `Validating task for block number: ${blockNumber}, Task Performer: ${performer}, Elected Performer: ${electedPerformer}`
+  );
 
-    let isValid = performer === electedPerformer; // Verify the performer is the elected performer
+  let isValid = performer === electedPerformer; // Verify the performer is the elected performer
 
-    res.status(200);
-    res.json({
-        data: isValid,
-        error: false,
-        message: "Success"
-    });
+  res.status(200);
+  res.json({
+    data: isValid,
+    error: false,
+    message: "Success",
+  });
 });
 
-app.listen(port, () => { console.log(`AVS Implementation listening on localhost:${port}`); });
+app.listen(port, () => {
+  console.log(`AVS Implementation listening on localhost:${port}`);
+});
